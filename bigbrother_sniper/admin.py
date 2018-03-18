01fe54c4ebdd3724f9923f7d37d0a66f3102965d
= <=>
2018-03-19 08:42:30 +0900

    Initial commit

---
+++ bigbrother_sniper/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from bigbrother_sniper.models import (
+                                     ProfessorProfile,
+                                     StudentProfile,
+                                     Department,
+                                     ProfileImage,
+                                     TextGuardList,
+                                     LabelGuardList,
+                                     PostAlertMessageLog
+                                     )
+
+# Register your models here.
+#admin.site.register(ProfessorProfile)
+#admin.site.register(StudentProfile)
+#admin.site.register(Department)
+#admin.site.register(ProfileImage)
+admin.site.register(TextGuardList)
+admin.site.register(LabelGuardList)
+admin.site.register(PostAlertMessageLog)
